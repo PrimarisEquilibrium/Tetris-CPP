d0ab3fe658789e195a63837d476654ecbcaa3c1e
Kevin <110409671+PrimarisEquilibrium@users.noreply.github.com>
2024-07-18 14:45:28 -0400

    Created the basic constants and data for the Tetris grid

--- Tetris/src/main.cpp
+++ Tetris/src/main.cpp
@@ -1,5 +1,6 @@
 #include "SFML/Graphics.hpp"
 #include <iostream>
+#include <vector>
 
 /**
  * @brief Gets the FPS of the window.
@@ -13,25 +14,66 @@ float getFps(sf::Clock& clock)
     return fps;
 }
 
-int main()
+// The different types of tetrominoes
+enum Block
 {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
-    sf::Event e;
-    sf::Clock clock; // Starts the clock
+    Empty,
+    I,
+    J,
+    L,
+    O,
+    S,
+    T,
+    Z
+};
 
+// Tetris grid dimension
+const int rows = 16;
+const int cols = 10;
+
+// Tile size of each square on the grid
+const int tileSize = 50;
+
+// Window dimensions
+const int width = cols * tileSize;
+const int height = rows * tileSize;
+
+// Colors
+const sf::Color bgColor(40, 40, 43);
+const sf::Color primaryColor(61, 61, 64);
+
+// 2D Vector containing the state of all tiles on the Tetris grid
+std::vector<std::vector<Block>> grid(rows, std::vector<Block>(cols, Block::Empty));
+
+int main()
+{       
+    // SFML variables
+    sf::RenderWindow window(sf::VideoMode(width, height), "SFML window");
+    sf::Clock clock;
+
+    // Forces the window to run at 60fps max
     window.setFramerateLimit(60);
+
+    // Run the program as long as the window is open
     while (window.isOpen())
     {
         std::cout << "Fps: " << getFps(clock) << std::endl;
+
+        // Check for any new events since the last iteration of the loop
+        sf::Event e;
         while (window.pollEvent(e))
         {
+            // Compare the received event with the "closing requested" event
             if (e.type == sf::Event::Closed)
             {
                 window.close();
             }
         }
 
-        window.clear();
+        // Clear the window with matte black color
+        window.clear(bgColor);
+
+        // End the current frame
         window.display();
     }
 
