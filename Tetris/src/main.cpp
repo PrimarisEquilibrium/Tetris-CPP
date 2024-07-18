ffa0045be8b1bd31f6d542f535e9c3efccc02fd7
Kevin <110409671+PrimarisEquilibrium@users.noreply.github.com>
2024-07-18 12:11:59 -0400

    getFps() made to practice sf::Clock and sf::Time

--- Tetris/src/main.cpp
+++ Tetris/src/main.cpp
@@ -1,12 +1,28 @@
 #include "SFML/Graphics.hpp"
+#include <iostream>
+
+/**
+ * @brief Gets the FPS of the window.
+ * @param clock an SFML clock.
+ * @return The FPS of the window as a float.
+ */
+float getFps(sf::Clock& clock)
+{
+    float currentTime = clock.restart().asSeconds();
+    float fps = 1.0f / currentTime;
+    return fps;
+}
 
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
     sf::Event e;
+    sf::Clock clock; // Starts the clock
 
+    window.setFramerateLimit(60);
     while (window.isOpen())
     {
+        std::cout << "Fps: " << getFps(clock) << std::endl;
         while (window.pollEvent(e))
         {
             if (e.type == sf::Event::Closed)
@@ -14,6 +30,9 @@ int main()
                 window.close();
             }
         }
+
+        window.clear();
+        window.display();
     }
 
     return 0;
