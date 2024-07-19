c81bf343d51e63053659e4ae6d0b6c50a062c3bc
Kevin <110409671+PrimarisEquilibrium@users.noreply.github.com>
2024-07-19 11:38:18 -0400

    Constants are now defined as inline - Fundamental constant types are now constexpr.

--- Tetris/include/constants.h
+++ Tetris/include/constants.h
@@ -4,16 +4,16 @@
 #include "SFML/Graphics.hpp"
 
 // Tetris grid dimension
-const int rows = 16;
-const int cols = 10;
-const float gridBorder = 1.f;
+inline constexpr int rows = 16;
+inline constexpr int cols = 10;
+inline constexpr float gridBorder = 1.f;
 
 // Tile size of each square on the grid
-const int tileSize = 50;
+inline constexpr int tileSize = 50;
 
 // Window dimensions
-const int width = cols * tileSize;
-const int height = rows * tileSize;
+inline constexpr int width = cols * tileSize;
+inline constexpr int height = rows * tileSize;
 
 // Colors
 const sf::Color bgColor(40, 40, 43);
